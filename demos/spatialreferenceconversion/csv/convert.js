efd90cd36c2615b8c62685b8809e17b8e5de85b7
Patrick Arlt <patrick.arlt@gmail.com>
2014-07-31 08:41:53 -0700

    final fixes

--- demos/spatialreferenceconversion/csv/convert.js
+++ demos/spatialreferenceconversion/csv/convert.js
@@ -2,9 +2,16 @@ var proj4 = require('proj4');
 var csv = require('csv');
 var fs = require('fs');
 
+// http://spatialreference.org/ref/esri/102718/
+// http://spatialreference.org/ref/esri/102718/proj4js/
 var nystateplane = '+proj=lcc +lat_1=40.66666666666666 +lat_2=41.03333333333333 +lat_0=40.16666666666666 +lon_0=-74 +x_0=300000 +y_0=0 +ellps=GRS80 +datum=NAD83 +to_meter=0.3048006096012192 +no_defs';
+
+// http://spatialreference.org/ref/epsg/4326/
+// http://spatialreference.org/ref/epsg/4326/proj4js/
 var latlng = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs';
 
+// Empty GeoJSON Feature Collection
+// http://geojson.org/geojson-spec.html#feature-collection-objects
 var graffiti = {
   type: 'FeatureCollection',
   features: []
@@ -20,7 +27,7 @@ fs.createReadStream('Graffiti_Locations.csv')
         type: 'Feature',
         geometry: {
           type: 'Point',
-          coordinates:proj4(nystateplane, latlng,[data.x,data.y])
+          coordinates:proj4(nystateplane, latlng, [data.x,data.y])
         },
         properties: data
       });
